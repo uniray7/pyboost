9307c12e902587b211f824dabd8ddb456db48e1a
uniray7 <uniray7@gmail.com>
2016-11-16 06:59:28 +0000

    fix indent and delete standalone converter

--- src/python_module.cpp
+++ src/python_module.cpp
@@ -5,7 +5,7 @@
 #include "opencv2/imgproc.hpp"
 
 #include <boost/python.hpp>
-#include <pyboostcvconverter/pyboostcvconverter.hpp>
+#include <pyboostcvconverter.hpp>
 
 #include <iostream>
 #include <typeinfo>
@@ -13,91 +13,64 @@
 
 namespace pbcvt {
 
-    using namespace boost::python;
-    using namespace cv;
+  using namespace boost::python;
+  using namespace cv;
 
 
-    PyObject *grayscale(PyObject *py_img) {
-        cv::Mat cv_img;
-        cv_img = pbcvt::fromNDArrayToMat(py_img);
+  cv::Mat grayscale(cv::Mat cv_img) {
+    cv::Mat gray_result;
+    cvtColor(cv_img, gray_result, COLOR_BGR2GRAY);
+    return gray_result;
+  }
 
-        cv::Mat gray_result;
-        cvtColor(cv_img, gray_result, COLOR_BGR2GRAY);
-        PyObject *ret = pbcvt::fromMatToNDArray(gray_result);
-        return ret;
+
+
+  //This example uses Mat directly, but we won't need to worry about the conversion
+  /**
+   * Example function. Basic inner matrix product using implicit matrix conversion.
+   * @param leftMat left-hand matrix operand
+   * @param rightMat right-hand matrix operand
+   * @return an NdArray representing the dot-product of the left and right operands
+   */
+  cv::Mat dot2(cv::Mat leftMat, cv::Mat rightMat) {
+    auto c1 = leftMat.cols, r2 = rightMat.rows;
+    if (c1 != r2) {
+      PyErr_SetString(PyExc_TypeError,
+          "Incompatible sizes for matrix multiplication.");
+      throw_error_already_set();
     }
+    cv::Mat result = leftMat * rightMat;
 
-
-/**
- * Example function. Basic inner matrix product using explicit matrix conversion.
- * @param left left-hand matrix operand (NdArray required)
- * @param right right-hand matrix operand (NdArray required)
- * @return an NdArray representing the dot-product of the left and right operands
- */
-    PyObject *dot(PyObject *left, PyObject *right) {
-
-        cv::Mat leftMat, rightMat;
-        leftMat = pbcvt::fromNDArrayToMat(left);
-        rightMat = pbcvt::fromNDArrayToMat(right);
-        auto c1 = leftMat.cols, r2 = rightMat.rows;
-        // Check that the 2-D matrices can be legally multiplied.
-        if (c1 != r2) {
-            PyErr_SetString(PyExc_TypeError,
-                            "Incompatible sizes for matrix multiplication.");
-            throw_error_already_set();
-        }
-        cv::Mat result = leftMat * rightMat;
-        PyObject *ret = pbcvt::fromMatToNDArray(result);
-        return ret;
-    }
-
-//This example uses Mat directly, but we won't need to worry about the conversion
-/**
- * Example function. Basic inner matrix product using implicit matrix conversion.
- * @param leftMat left-hand matrix operand
- * @param rightMat right-hand matrix operand
- * @return an NdArray representing the dot-product of the left and right operands
- */
-    cv::Mat dot2(cv::Mat leftMat, cv::Mat rightMat) {
-        auto c1 = leftMat.cols, r2 = rightMat.rows;
-        if (c1 != r2) {
-            PyErr_SetString(PyExc_TypeError,
-                            "Incompatible sizes for matrix multiplication.");
-            throw_error_already_set();
-        }
-        cv::Mat result = leftMat * rightMat;
-
-        return result;
-    }
+    return result;
+  }
 
 
 #if (PY_VERSION_HEX >= 0x03000000)
 
-    static void *init_ar() {
+  static void *init_ar() {
 #else
-        static void init_ar(){
+    static void init_ar(){
 #endif
-        Py_Initialize();
+      Py_Initialize();
 
-        import_array();
-        return NUMPY_IMPORT_ARRAY_RETVAL;
+      import_array();
+      return NUMPY_IMPORT_ARRAY_RETVAL;
     }
 
     BOOST_PYTHON_MODULE (pbcvt) {
-        //using namespace XM;
-        init_ar();
+      //using namespace XM;
+      init_ar();
 
-        //initialize converters
-        to_python_converter<cv::Mat,
-                pbcvt::matToNDArrayBoostConverter>();
-        pbcvt::matFromNDArrayBoostConverter();
+      //initialize converters
+      to_python_converter<cv::Mat,
+        pbcvt::matToNDArrayBoostConverter>();
+      pbcvt::matFromNDArrayBoostConverter();
 
-        //expose module-level functions
-        def("dot", dot);
-        def("dot2", dot2);
-        def("grayscale", grayscale);
+      //expose module-level functions
+      def("dot2", dot2);
+      def("grayscale", grayscale);
 
 
     }
 
-} //end namespace pbcvt
+  } //end namespace pbcvt
